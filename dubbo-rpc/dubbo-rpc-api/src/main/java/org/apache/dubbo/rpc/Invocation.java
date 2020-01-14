b1bcb4f8364eb16096c3228f724882f264b245de
dufeng3 <dufeng3@xiaomi.com>
2020-01-14 16:12:10 +0800

    增加领域模型注释

--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/Invocation.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/Invocation.java
@@ -21,7 +21,11 @@ import java.util.stream.Stream;
 
 /**
  * Invocation. (API, Prototype, NonThreadSafe)
- *
+ * 在Dubbo的核心领域模型中：
+ *  Protocol是服务域，它是Invoker暴露和引用的主功能入口，它负责Invoker的生命周期管理。
+ *  Invoker是实体域，它是Dubbo的核心模型，其它模型都向它靠拢，或转换成它，它代表一个可执行体，可向它发起invoke调用，它有可能是一个本地的实现，
+ *  也可能是一个远程的实现，也可能是一个集群的实现。
+ *  Invocation是会话域，它持有调用过程中的变量，比如方法名，参数等。
  * @serial Don't change the class name and package name.
  * @see org.apache.dubbo.rpc.Invoker#invoke(Invocation)
  * @see org.apache.dubbo.rpc.RpcInvocation
--- dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/Invoker.java
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/Invoker.java
@@ -21,6 +21,11 @@ import org.apache.dubbo.common.Node;
 /**
  * Invoker. (API/SPI, Prototype, ThreadSafe)
  *
+ * 在Dubbo的核心领域模型中：
+ *  Protocol是服务域，它是Invoker暴露和引用的主功能入口，它负责Invoker的生命周期管理。
+ *  Invoker是实体域，它是Dubbo的核心模型，其它模型都向它靠拢，或转换成它，它代表一个可执行体，可向它发起invoke调用，它有可能是一个本地的实现，
+ *  也可能是一个远程的实现，也可能是一个集群的实现。
+ *  Invocation是会话域，它持有调用过程中的变量，比如方法名，参数等。
  * @see org.apache.dubbo.rpc.Protocol#refer(Class, org.apache.dubbo.common.URL)
  * @see org.apache.dubbo.rpc.InvokerListener
  * @see org.apache.dubbo.rpc.protocol.AbstractInvoker
